99161f7fce82d534dd75ee31d25b4f9e48953e94
Regina Furness <skritschy@gmail.com>
2020-04-15 19:37:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,5 @@
 def reverse_each_word(string)
   new_string=[]
-  array = string.split(//)
-  array.each {|word| new_string << word.reverse}
+  string.split.each {|word| new_string << word.reverse}
   new_string.join
 end
